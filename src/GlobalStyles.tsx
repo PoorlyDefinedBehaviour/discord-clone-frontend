58ddefc1de66b7196244c573abcd31dc67e3f573
Bruno <brunotj2015@hotmail.com>
2019-08-10 00:37:18 -0300

    :lipstick: adds default width and height to body

--- src/GlobalStyles.tsx
+++ src/GlobalStyles.tsx
@@ -1,7 +1,13 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
+html, body {
+  width: 100%;
+  height: 100vh;
+}
+
 * {
+  
   @import url("https://fonts.googleapis.com/css?family=Montserrat&display=swap");
   font-family: 'Montserrat', sans-serif;
 }
